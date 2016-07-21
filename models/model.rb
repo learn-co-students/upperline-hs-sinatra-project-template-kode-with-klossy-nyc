11012372fdc2de3f183012ada11c7a4e3513836d
Martha Czernuszenko <mvczer@gmail.com>
2016-07-21 17:56:08 +0000

    app controller

--- models/model.rb
+++ models/model.rb
@@ -1,9 +1,21 @@
-def new_york_city
-  new_york_city=["nyc_1", "nyc_2", "nyc_3", "nyc_4", "nyc_5"]
-end
-  puts new_york_city
+def packing_list(location)
+  if location=="NYC"
+  return ["nyc_1" ,"nyc_2", "nyc_3","nyc_4","nyc_5"]
 
-def paris
-  paris=["paris_1", "paris_2", "paris_3", "paris_4", "paris_5"]
+  elsif location=="Paris"
+    return ["paris_1","paris_2","paris_3","paris_4", "paris_5"]
+  end
 end
-  puts paris
+#  puts city("new_york_city")
+#  puts city("paris")
+# puts "Please enter a city"
+# answer=gets.chomp
+# def city(input) #use a general term like input so the method is veratile
+#  #rubone qual assigns the value
+# if input == "New York" #2thingsthe same, 2 equal signs to check the value
+# "nyc_1 ,nyc_2, nyc_3,nyc_4,nyc_5"
+# elsif input== "Paris"
+# "paris_1,paris_2,paris_3,paris_4, paris_5"
+# end
+# end
+# puts city(answer)
