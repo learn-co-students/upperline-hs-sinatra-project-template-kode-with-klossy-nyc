3e15f605abee48e495727c1e9969367f1093e19d
Tracy Ma <mtracy.vp@gmail.com>
2016-07-21 18:43:11 +0000

    hello

--- application_controller.rb
+++ application_controller.rb
@@ -1,5 +1,6 @@
 require 'bundler'
 Bundler.require
+require_relative 'models/model.rb'
 
 class ApplicationController < Sinatra::Base
 
@@ -7,4 +8,25 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get '/q2' do
+    erb :index2
+  end
+
+  get '/results' do
+    erb :results
+  end
+
+  post '/choice' do
+    @area = params[:neighborhood]
+    @type = params[:type]
+    erb :results
+  end
+
+  post '/results' do
+    @type = params[:type]
+    puts @area
+    puts @area.class
+    @recommendation = recommend(@area, @type)
+    erb :results
+  end
 end
